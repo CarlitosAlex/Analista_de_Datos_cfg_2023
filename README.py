7019585ee5745f8621299f1d5aeb52c677ecf617
CarlitosAlex <110439283+CarlitosAlex@users.noreply.github.com>
2023-02-01 14:25:07 -0300

    Add files via upload

--- README.py
+++ README.py
@@ -1,3 +1,8 @@
 El objetivo de este trabajo, es aportar información extraída del análisis de datos entregados por el cliente para la toma de decisiones, ellos desean entrar en el mercado de cursos de capacitación en línea y sus objetivos son, dar a conocer la marca a su público objetivo, atención de calidad a sus clientes y buenos niveles de rentabilidad.
 Lo primero que se realizo fue el proceso exploratorio de los datos, corrigendo datos nulos y preparando los sets de datos para el análisis.
 Luego se cargaron los sets de datos en Power BI donde se comunican los resultados del análisis realizado en un dashboard interactivo y un informe.
+
+link_Dashboard: https://drive.google.com/file/d/1aRxd22lRaMvU67IW7oJtwP6j3zBfiMcl/view?usp=share_link
+
+Bibliografía: 
+https://www.iebschool.com/blog/tendencias-e-learning-innovacion/ 
